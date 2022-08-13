03bf475efe1c3a85fb4845ce12ee6f772f8543af
Mohammed Ali <wedhimd@gmail.com>
2022-08-13 11:49:12 +0200

    add enviroment vairables

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,16 +1,16 @@
 export const config = {
   dev: {
-    username: "dbudigramdev",
-    password: "dbudigramdev",
-    database: "dbudigramdev",
-    host: "dbudigramdev.c20s2o9fdl2c.us-east-1.rds.amazonaws.com",
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: "postgres",
-    aws_region: "us-east-1",
-    aws_profile: "default",
-    aws_media_bucket: "udigramalidev",
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_MEDIA_BUCKET,
   },
   jwt: {
-    secret: " ",
+    secret: process.env.JWT_SECRET,
   },
   prod: {
     username: "",
