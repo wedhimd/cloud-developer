133b0d5857ceddca4d463976c4528458ae39d63b
Mohammed Ali <wedhimd@gmail.com>
2022-08-13 10:55:04 +0200

    after first patch

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,22 +1,22 @@
 export const config = {
-  "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
-    "dialect": "postgres",
-    "aws_region": "us-east-2",
-    "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+  dev: {
+    username: "dbudigramdev",
+    password: "dbudigramdev",
+    database: "dbudigramdev",
+    host: "dbudigramdev.c20s2o9fdl2c.us-east-1.rds.amazonaws.com",
+    dialect: "postgres",
+    aws_region: "us-east-1",
+    aws_profile: "default",
+    aws_media_bucket: "udigramalidev",
   },
-  "jwt": {
-    "secret": " "
+  jwt: {
+    secret: " ",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
-  }
-}
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+  },
+};
